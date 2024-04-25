e669d629bad17ff5e162c79266f125a24de48f60
Timothy Williams <7538854+tim-br@users.noreply.github.com>
2024-04-25 03:36:24 -0700

    Add readme.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import FileUpload from './FileUpload';
 //import Navbar from './NavBar';
@@ -7,6 +6,15 @@ function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
+      <header className="app-header">
+        <h1>FileUpload</h1>
+        <a href="https://github.com/tim-br/file-upload"
+          className="github-link"
+          target="_blank"
+          rel="noopener noreferrer">
+          GitHub Repo
+        </a>
+      </header>
       <AuthenticationButton />
       <FileUpload />
     </div>
